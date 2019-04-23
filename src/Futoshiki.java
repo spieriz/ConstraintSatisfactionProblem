b446f10e0b7a196131eb0da1b8217903c2293294
spieriz <spieriz@gmail.com>
2019-04-23 12:41:47 +0200

    Added arrayList with restrictions in futoshiki class.

--- src/Futoshiki.java
+++ src/Futoshiki.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Futoshiki {
     private int[][] board;
     private int dimensions;
+    private ArrayList<Restriction> restrictions;
 
     Futoshiki(int dimensions){
         this.dimensions = dimensions;
