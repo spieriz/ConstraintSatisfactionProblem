eb4feebcac839fea5253976453f279362ca1a342
spieriz <spieriz@gmail.com>
2019-04-23 12:57:35 +0200

    Added setters to restrictions.

--- src/Restriction.java
+++ src/Restriction.java
@@ -5,6 +5,14 @@ public class Restriction {
     private int rowBigger;
     private int columnBigger;
 
+    Restriction() {
+        this.rowSmaller = -1;
+        this.columnSmaller = -1;
+
+        this.rowBigger = -1;
+        this.columnBigger = -1;
+    }
+
     Restriction(int rowSmaller, int columnSmaller, int rowBigger, int columnBigger) {
         this.rowSmaller = rowSmaller;
         this.columnSmaller = columnSmaller;
@@ -17,15 +25,31 @@ public class Restriction {
         return rowSmaller;
     }
 
+    public void setRowSmaller(int rowSmaller) {
+        this.rowSmaller = rowSmaller;
+    }
+
     public int getColumnSmaller() {
         return columnSmaller;
+    }
+
+    public void setColumnSmaller(int columnSmaller) {
+        this.columnSmaller = columnSmaller;
     }
 
     public int getRowBigger() {
         return rowBigger;
     }
 
+    public void setRowBigger(int rowBigger) {
+        this.rowBigger = rowBigger;
+    }
+
     public int getColumnBigger() {
         return columnBigger;
+    }
+
+    public void setColumnBigger(int columnBigger) {
+        this.columnBigger = columnBigger;
     }
 }
