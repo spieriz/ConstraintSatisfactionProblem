96bb9c5f9634533871cde301240a7de6269b4c6a
spieriz <spieriz@gmail.com>
2019-04-23 15:10:45 +0200

    Added methods to check all restrictions: uniqueness and relations.

--- src/Futoshiki.java
+++ src/Futoshiki.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Futoshiki {
     private int[][] board;
@@ -23,6 +25,56 @@ public class Futoshiki {
     }
 
     boolean checkIfBoardMeetsRestrictions() {
+        return checkRelationsRestrictions() && checkUniquenessRestrictions();
+    }
+
+    private boolean checkUniquenessRestrictions() {
+        return checkUniquenessRows() && checkUniquenessColumns();
+    }
+
+    private boolean checkUniquenessRows() {
+        boolean meetsRestrictions = true;
+
+        for (int row = 0; row < dimensions && meetsRestrictions; row++) {
+            Set<Integer> itemsSet = new TreeSet<>();
+            int zeros = 0;
+
+            for (int column = 0; column < dimensions; column++) {
+                if (board[row][column] != 0)
+                    itemsSet.add(board[row][column]);
+                else
+                    zeros++;
+            }
+
+            if (itemsSet.size() != dimensions - zeros) {
+                meetsRestrictions = false;
+            }
+        }
+        return meetsRestrictions;
+    }
+
+    private boolean checkUniquenessColumns() {
+        boolean meetsRestrictions = true;
+
+        for (int column = 0; column < dimensions && meetsRestrictions; column++) {
+            Set<Integer> itemsSet = new TreeSet<>();
+            int zeros = 0;
+
+            for (int row = 0; row < dimensions; row++) {
+                if (board[row][column] != 0)
+                    itemsSet.add(board[row][column]);
+                else
+                    zeros++;
+            }
+
+            if (itemsSet.size() != dimensions - zeros) {
+                meetsRestrictions = false;
+            }
+        }
+        return meetsRestrictions;
+    }
+
+    private boolean checkRelationsRestrictions() {
         boolean meetsRestrictions = true;
 
         for (int i = 0; i < restrictions.size() && meetsRestrictions; i++) {
