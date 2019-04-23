8e36560c85be56ea157a4e9482b5ca0d785161b7
spieriz <spieriz@gmail.com>
2019-04-23 21:33:57 +0200

    Added method to use futoshiki backtracking calculation.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args){
+        calculateFutoshikiBacktracking();
+    }
 
+    private static void calculateFutoshikiBacktracking() {
+        String filename = "files/test_futo_6_0.txt";
+
+        FileParser fileParser = new FileParser(new File(filename), FileParser.CSP_TYPE.FUTOSHIKI);
+
+        fileParser.parseHeader();
+        fileParser.parseFutoshikiFile();
+
+        Futoshiki futoshiki = fileParser.getFutoshiki();
+        System.out.println(futoshiki.restrictionsToString());
+        System.out.println(futoshiki.checkIfBoardMeetsRestrictions(futoshiki.getBoard()));
+
+        futoshiki.setBoard(futoshiki.calculateFutoshikiBacktracking(futoshiki.getBoard(), 0));
+        int[][] board = futoshiki.getBoard();
+        System.out.println("\n\nlocal board:\n" + futoshiki.boardToString(board));
+        System.out.println("relations: " + futoshiki.checkRelationsRestrictions(board));
+        System.out.println(futoshiki.isCompleted(board));
     }
 }
